2a57edaeb799b07276cec9b5122c9f26ed953988
minimaded <66683862+minimaded@users.noreply.github.com>
2022-04-04 20:46:04 +0100

    Add files via upload

--- run_backup.sh
+++ run_backup.sh
@@ -84,7 +84,7 @@ _sleep() {
 log_file() {
     while read -r line; do
         echo "${line}"
-        echo "${line}" | sed 's/\x1b\[[0-9;]*m\|\x1b[(]B\x1b\[m//g' > /dev/null | sudo tee -a "/mnt/ExtSSD/BackUp/test.log"| sudo tee -a "${backup_saveas}.log"
+        echo "${line}" | sed 's/\x1b\[[0-9;]*m\|\x1b[(]B\x1b\[m//g' > /dev/null | sudo tee -a "${backup_saveas}.log"
     done
 }
 
