665c59ecd2974322cca8d5bb0bf21ab74f6fd14a
minimaded <66683862+minimaded@users.noreply.github.com>
2022-04-04 19:39:27 +0100

    Add files via upload

--- run_backup.sh
+++ run_backup.sh
@@ -367,6 +367,6 @@ compress_zip() {
 _colors
 script_path="$( readlink -f "$0" )"
 user_name="$( sudo ls "/home" | tail -n 1 )"
-_status 0 "Username is ${user_name}" | log_file
+_status 0 "Username is ${user_name}" | tee -a | log_file
 parse_params "$@"
-do_all | log_file
+do_all | tee -a | log_file
