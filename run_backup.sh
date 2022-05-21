d64be3e23040bb80021f3f562db40513a5fd59aa
minimaded <66683862+minimaded@users.noreply.github.com>
2022-05-21 17:59:39 +0100

    Add files via upload

--- run_backup.sh
+++ run_backup.sh
@@ -271,10 +271,14 @@ parse_params() {
 
 check_tools() {
     _status 3 "Checking for required tools"
+	echo "test 1"
     req_tools="parted losetup tune2fs md5sum e2fsck resize2fs"
     for command in $req_tools; do
+	echo "test 2"
         command -v "${command}" >/dev/null 2>&1
+		echo "test 3"
         if (( $? != 0 )); then
+		echo "test 4"
             _query "${command} is required, press [y/N] to install... "
             case "$response" in
                 [yY][eE][sS]|[yY])
