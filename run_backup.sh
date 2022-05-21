0d1b3ec7e18f20e217525b8e170e15602b0bc2ff
minimaded <66683862+minimaded@users.noreply.github.com>
2022-05-21 05:26:46 +0100

    Add files via upload

--- run_backup.sh
+++ run_backup.sh
@@ -29,7 +29,7 @@ _done() {
     fi
     echo
     echo -n "Press any key to exit..."
-    read -r -n1
+    read -r -n 1
     echo
     exit 0
 }
@@ -37,7 +37,7 @@ _done() {
 _notdone() {
     echo
     echo -n "${script_name} failed...Press any key to exit"
-    read -r -n1
+    read -r -n 1
     echo
     exit 1
 }
